48e99ea723752216a4af800f8b1381182de47fe6
Harry Bawsac <harry@bawsac.com>
2022-04-12 23:00:26 +0200

    Add i18n

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-const withNextra = require("nextra")({
-	theme: "nextra-theme-docs",
-	themeConfig: "./theme.config.js",
-	unstable_staticImage: true,
-});
+// const withNextra = require("nextra")({
+// 	theme: "nextra-theme-docs",
+// 	themeConfig: "./theme.config.js",
+// 	unstable_staticImage: true,
+// 	i18n: {
+// 		locales: ['en', 'nl'],
+// 		defaultLocale: 'en',
+// 	},
+// });
 
-module.exports = withNextra();
+// module.exports = withNextra();
+
+const withNextra = require("nextra")("nextra-theme-docs", "./theme.config.js");
+
+module.exports = withNextra({
+	unstable_staticImage: true,
+	i18n: {
+		locales: ['en', 'nl'],
+		defaultLocale: 'en'
+	}
+});
--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,16 @@
 export default {
-	github: "https://github.com/shuding/nextra",
-	docsRepositoryBase: "https://github.com/shuding/nextra/blob/master",
-	titleSuffix: " – Nextra",
+	github: "https://github.com/harrybawsac/theroadtonode",
+	docsRepositoryBase: "https://github.com/harrybawsac/theroadtonode/blob/master",
+	titleSuffix: " – The Road to Node",
+	i18n: [
+		{ locale: 'en', text: 'English' },
+		{ locale: 'nl', text: 'Dutch' }
+	],
 	logo: (
 		<>
-			<span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+			<span className="mr-2 font-extrabold hidden md:inline">The Road to Node</span>
 			<span className="text-gray-600 font-normal hidden md:inline">
-				The Next.js Static Site Generator
+				A Bitcoin and Lightning node guide
 			</span>
 		</>
 	),
@@ -16,15 +20,15 @@ export default {
 			<meta name="theme-color" content="#ffffff" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<meta httpEquiv="Content-Language" content="en" />
-			<meta name="description" content="Nextra: the Next.js site builder" />
-			<meta name="og:description" content="Nextra: the Next.js site builder" />
+			<meta name="description" content="The Road to Node: A Bitcoin and Lightning node guide" />
+			<meta name="og:description" content="The Road to Node: A Bitcoin and Lightning node guide" />
 			<meta name="twitter:card" content="summary_large_image" />
 			<meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-			<meta name="twitter:site:domain" content="nextra.vercel.app" />
-			<meta name="twitter:url" content="https://nextra.vercel.app" />
-			<meta name="og:title" content="Nextra: Next.js static site generator" />
+			<meta name="twitter:site:domain" content="https://github.com/harrybawsac/theroadtonode" />
+			<meta name="twitter:url" content="https://github.com/harrybawsac/theroadtonode" />
+			<meta name="og:title" content="The Road to Node: A Bitcoin and Lightning node guide" />
 			<meta name="og:image" content="https://nextra.vercel.app/og.png" />
-			<meta name="apple-mobile-web-app-title" content="Nextra" />
+			<meta name="apple-mobile-web-app-title" content="The Road to Node" />
 			<link
 				rel="apple-touch-icon"
 				sizes="180x180"
@@ -62,6 +66,6 @@ export default {
 	nextLinks: true,
 	footer: true,
 	footerEditLink: "Edit this page on GitHub",
-	footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+	footerText: <>{new Date().getFullYear()} © The Road to Node.</>,
 	unstable_faviconGlyph: "👋",
 }
