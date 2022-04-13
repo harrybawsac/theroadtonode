0f655fe0b2696bee3add5a6f8f24639638832013
Harry Bawsac <harry@bawsac.com>
2022-04-13 22:11:34 +0200

    Add introduction, raspberry pi and bitcoin core

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-// const withNextra = require("nextra")({
-// 	theme: "nextra-theme-docs",
-// 	themeConfig: "./theme.config.js",
-// 	unstable_staticImage: true,
-// 	i18n: {
-// 		locales: ['en', 'nl'],
-// 		defaultLocale: 'en',
-// 	},
-// });
-
-// module.exports = withNextra();
-
 const withNextra = require("nextra")("nextra-theme-docs", "./theme.config.js");
 
 module.exports = withNextra({
-	unstable_staticImage: true,
 	i18n: {
 		locales: ['en', 'nl'],
 		defaultLocale: 'en'
