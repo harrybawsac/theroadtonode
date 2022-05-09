a4465841f259c04398da9dc3efa11a6f643e6d6e
Harry Bawsac <harry@bawsac.com>
2022-05-09 13:45:52 +0200

    Fixed some minor inconsistencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import "nextra-theme-docs/style.css";
+import { useEffect } from "react";
 
 export default function Nextra({ Component, pageProps }) {
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [pageProps])
+
 	return <Component {...pageProps} />;
 };
